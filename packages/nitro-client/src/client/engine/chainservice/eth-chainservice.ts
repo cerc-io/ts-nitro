854bf8628a631441eac0369dd9f8124795f3b58e
nabarun <nabarun@deepstacksoft.com>
2023-05-22 18:56:25 +0530

    Add empty methods for EthChainService class

--- packages/nitro-client/src/client/engine/chainservice/eth-chainservice.ts
+++ packages/nitro-client/src/client/engine/chainservice/eth-chainservice.ts
@@ -1,15 +1,22 @@
-import { AddressLike, TransactionLike } from 'ethers';
+import { AddressLike, Log, TransactionLike } from 'ethers';
 import debug from 'debug';
 
 import { NitroAdjudicator } from './adjudicator/nitro-adjudicator';
 import { Event } from './chainservice';
-import { GoChannelPlaceholder } from '../../../go-channel';
+import { GoChannelPlaceholder, GoReceivingChannelPlaceholder } from '../../../go-channel';
+import { ChainTransaction } from '../../../protocols/interfaces';
 
 interface EthChain {
   // TODO: Extend bind.ContractBackend (github.com/ethereum/go-ethereum/accounts/abi/bind)
   // TODO: Extend ethereum.TransactionReader (github.com/ethereum/go-ethereum)
 
-  chainID: (ctx: AbortController) => [bigint, Error]
+  // TODO: Can throw an error
+  chainID: (ctx: AbortController) => bigint
+}
+
+interface BlockRange {
+  from: bigint;
+  to: bigint;
 }
 
 export class EthChainService {
@@ -67,4 +74,46 @@ export class EthChainService {
     vpaAddress: AddressLike,
     logDestination: WritableStream,
   ): EthChainService | void {}
+
+  // defaultTxOpts returns transaction options suitable for most transaction submissions
+  // TODO: Implement and remove void
+  private defaultTxOpts(): TransactionLike | void {}
+
+  // sendTransaction sends the transaction and blocks until it has been submitted.
+  // TODO: Implement and remove void
+  sendTransaction(tx: ChainTransaction): Error | void {}
+
+  // fatalF is called to output a message and then panic, killing the chain service.
+  // TODO: Implement
+  private fatalF(format: string, ...v: any[]) {}
+
+  // dispatchChainEvents takes in a collection of event logs from the chain
+  // TODO: Implement
+  private dispatchChainEvents(logs: Log[]) {}
+
+  // subscribeForLogs subscribes for logs and pushes them to the out channel.
+  // It relies on notifications being supported by the chain node.
+  // TODO: Implement
+  private subscribeForLogs() {}
+
+  // splitBlockRange takes a BlockRange and chunks it into a slice of BlockRanges, each having an interval no larger than the passed interval.
+  // TODO: Implement and remove void
+  private splitBlockRange(total: BlockRange, maxInterval: bigint): BlockRange[] | void {}
+
+  // eventFeed returns the out chan, and narrows the type so that external consumers may only receive on it.
+  // TODO: Implement and remove void
+  eventFeed(): GoReceivingChannelPlaceholder<Event> | void {}
+
+  // TODO: Implement and remove void
+  getConsensusAppAddress(): AddressLike | void {}
+
+  // TODO: Implement and remove void
+  getVirtualPaymentAppAddress(): AddressLike | void {}
+
+  // TODO: Implement and remove void
+  // TODO: Can throw an error
+  getChainId(): bigint | void {}
+
+  // TODO: Implement and remove void
+  close(): Error | void {}
 }
