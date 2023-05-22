defb84b827a2f7362d0a79e8e59f8d002626e459
nabarun <nabarun@deepstacksoft.com>
2023-05-22 10:29:19 +0530

    Setup test with mocha in nitro-client

--- packages/nitro-client/src/index.ts
+++ packages/nitro-client/src/index.ts
@@ -1,6 +1,8 @@
-const test = () => {
+const test = (): string => {
   // eslint-disable-next-line no-console
   console.log('Test from nitro-client');
+
+  return 'test output';
 };
 
 export default test;
---
+++ packages/nitro-client/test/index.test.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import test from '../src/index';
+
+describe('testFunction', () => {
+  it('should return the expected result', () => {
+    const result = test();
+    expect(result).to.equal('test output');
+  });
+});
