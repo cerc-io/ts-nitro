f19202477af0260df39d8c9d440f6bbd86847af7
prathamesh0 <prathamesh.musale0@gmail.com>
2023-05-22 13:48:10 +0530

    Add methods to MessageService interface

--- packages/nitro-client/src/client/engine/messageservice/messageservice.ts
+++ packages/nitro-client/src/client/engine/messageservice/messageservice.ts
@@ -1,2 +1,19 @@
-// TODO: Add methods
-export interface MessageService {}
+import { GoReceivingChannelPlaceholder } from '../../../go-channel';
+import { Message } from '../../../protocols/messages';
+
+// TODO: Add p2p implementation
+// TODO: Add tests
+export interface MessageService {
+  // TODO: Update comments
+
+  // Out returns a chan for receiving messages from the message service
+  out (): GoReceivingChannelPlaceholder<Message>;
+
+  // Send is for sending messages with the message service
+  // TODO: Use protocols message type
+  send (msg: Message): void;
+
+  // Close closes the message service
+  // TODO: Can throw an error
+  close (): void;
+}
--- packages/nitro-client/src/index.ts
+++ packages/nitro-client/src/index.ts
@@ -1,8 +1,8 @@
-const test = (): string => {
+export const test = (): string => {
   // eslint-disable-next-line no-console
   console.log('Test from nitro-client');
 
   return 'test output';
 };
 
-export * as Client from './client/client';
+export { Client } from './client/client';
