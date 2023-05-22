ce81679372f41e3bad97999894554654851ac4f2
prathamesh0 <prathamesh.musale0@gmail.com>
2023-05-22 19:45:49 +0530

    Add placeholder methods for VoucherManager class

--- packages/nitro-client/src/client/client.ts
+++ packages/nitro-client/src/client/client.ts
@@ -1,8 +1,10 @@
+import { ethers } from 'ethers';
+
 import { MessageService } from './engine/messageservice/messageservice';
 import { ChainService } from './engine/chainservice/chainservice';
 import { Store } from './engine/store/store';
 import { PolicyMaker } from './engine/policy-maker';
-import { VoucherManager } from '../payments/voucher-manager';
+import { VoucherManager, VoucherStore } from '../payments/voucher-manager';
 import { Engine } from './engine/engine';
 
 export class Client {
@@ -12,7 +14,7 @@ export class Client {
   private vm: VoucherManager;
 
   constructor(msg: MessageService, chain: ChainService, store: Store, policymaker: PolicyMaker) {
-    this.vm = new VoucherManager();
+    this.vm = new VoucherManager(ethers.ZeroAddress, store as unknown as VoucherStore);
     this.engine = new Engine(this.vm, msg, chain, store, policymaker);
   }
 }
--- packages/nitro-client/src/client/engine/policy-maker.ts
+++ packages/nitro-client/src/client/engine/policy-maker.ts
@@ -1,2 +1,6 @@
-// TODO: Add methods
-export interface PolicyMaker {}
+import { Objective } from '../../protocols/interfaces';
+
+// PolicyMaker is used to decide whether to approve or reject an objective
+export interface PolicyMaker {
+  shouldApprove (o: Objective): boolean
+}
