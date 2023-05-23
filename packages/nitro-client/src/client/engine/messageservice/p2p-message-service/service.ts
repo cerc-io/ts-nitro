23490d9ed9ed29feab4d8e3175fc8ccfee2ac8c6
nabarun <nabarun@deepstacksoft.com>
2023-05-23 11:21:57 +0530

    Add empty methods for P2PMessageService

--- packages/nitro-client/src/client/engine/messageservice/p2p-message-service/service.ts
+++ packages/nitro-client/src/client/engine/messageservice/p2p-message-service/service.ts
@@ -9,8 +9,12 @@ import type { PrivateKey } from '@libp2p/crypto';
 import type { MulticastDNSComponents } from '@libp2p/mdns';
 // @ts-expect-error
 import type { PeerDiscovery } from '@libp2p/interface-peer-discovery';
+// @ts-expect-error
+import type { Stream } from '@libp2p/interface-connection';
+// @ts-expect-error
+import type { Multiaddr } from '@multiformats/multiaddr';
 
-import { GoChannelPlaceholder } from '../../../../go-channel';
+import { GoChannelPlaceholder, GoReceivingChannelPlaceholder } from '../../../../go-channel';
 import { Map } from '../../../../internal/safesync/safesync';
 import { Message } from '../../../../protocols/messages';
 import { Address } from '../../../../types/types';
@@ -19,6 +23,14 @@ import { Address } from '../../../../types/types';
 interface BasicPeerInfo {
   id: string;
   address: Address;
+}
+
+// PeerInfo contains peer information and the ip address/port
+interface PeerInfo {
+  port: number;
+  id: string;
+  address: Address;
+  ipAddress: string;
 }
 
 // P2PMessageService is a rudimentary message service that uses TCP to send and receive messages.
@@ -72,4 +84,51 @@ export class P2PMessageService {
     useMdnsPeerDiscovery: boolean,
     logWriter: WritableStream,
   ): P2PMessageService | void {}
+
+  // id returns the libp2p peer ID of the message service.
+  // TODO: Implement and remove void
+  id(): string | void {}
+
+  // handlePeerFound is called by the mDNS service when a peer is found.
+  // TODO: Implement and remove void
+  handlePeerFound(pi: Multiaddr[]) {}
+
+  // TODO: Implement
+  private msgStreamHandler(stream: Stream) {}
+
+  // sendPeerInfo sends our peer info over the given stream
+  // TODO: Implement
+  private sendPeerInfo(stream: Stream) {}
+
+  // receivePeerInfo receives peer info from the given stream
+  // TODO: Implement
+  private receivePeerInfo(stream: Stream) {}
+
+  // Sends messages to other participants.
+  // It blocks until the message is sent.
+  // It will retry establishing a stream NUM_CONNECT_ATTEMPTS times before giving up
+  // TODO: Implement
+  send(msg: Message) {}
+
+  // checkError panics if the message service is running and there is an error, otherwise it just returns
+  // TODO: Implement
+  // eslint-disable-next-line n/handle-callback-err
+  private checkError(err: Error) {}
+
+  // out returns a channel that can be used to receive messages from the message service
+  // TODO: Implement and remove void
+  out(): GoReceivingChannelPlaceholder<Message> | void {}
+
+  // Closes the P2PMessageService
+  // TODO: Implement and remove void
+  close(): Error | void {}
+
+  // peerInfoReceived returns a channel that receives a PeerInfo when a peer is discovered
+  // TODO: Implement and remove void
+  peerInfoReceived(): GoReceivingChannelPlaceholder<BasicPeerInfo> | void {}
+
+  // AddPeers adds the peers to the message service.
+  // We ignore peers that are ourselves.
+  // TODO: Implement
+  addPeers(peers: PeerInfo[]) {}
 }
