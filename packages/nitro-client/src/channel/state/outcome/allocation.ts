6fd45e21069829d6e4341598f81c0ae6c2dd58d1
prathamesh0 <prathamesh.musale0@gmail.com>
2023-05-24 12:00:19 +0530

    Add placeholder methods for Allocation class

--- packages/nitro-client/src/channel/state/outcome/allocation.ts
+++ packages/nitro-client/src/channel/state/outcome/allocation.ts
@@ -4,7 +4,6 @@ export enum AllocationType {
 }
 
 // Allocation declares an Amount to be paid to a Destination.
-// TODO: Implement
 export class Allocation {
   // Either an ethereum address or an application-specific identifier
   destination?: string;
@@ -17,6 +16,19 @@ export class Allocation {
 
   // Custom metadata (optional field, can be zero bytes). This can be used flexibly by different protocols.
   metadata?: Buffer;
+
+  // Equal returns true if the supplied Allocation matches the receiver Allocation, and false otherwise.
+  // Fields are compared with ==, except for big.Ints which are compared using Cmp
+  // TODO: Implement
+  equal(b: Allocation): boolean {
+    return false;
+  }
+
+  // Clone returns a deep copy of the receiver.
+  // TODO: Implement
+  clone(): Allocation {
+    return {} as Allocation;
+  }
 }
 
 // Allocations is an array of type Allocation
