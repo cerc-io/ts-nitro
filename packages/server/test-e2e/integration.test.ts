175660a46ec57c029344d42e7aee67c678ed604e
nabarun <nabarun@deepstacksoft.com>
2023-05-25 20:41:57 +0530

    Add script for running hardhat chain and separate e2e test

--- packages/server/test-e2e/integration.test.ts
+++ packages/server/test-e2e/integration.test.ts
@@ -10,12 +10,15 @@ import { createP2PMessageService } from '../src/utils';
 
 // Message service port
 const MESSAGING_PORT = 3005;
+
 // RPC endpoint for the chain
+// Chain should be running locally
 const CHAIN_URL = 'http://127.0.0.1:8545';
 
 describe('test Client', () => {
-  // TODO: Start local chain for test
-  xit('should instantiate Client', async () => {
+  let client: Client;
+
+  it('should instantiate Client', async () => {
     const messageService = await createP2PMessageService(MESSAGING_PORT);
 
     const chainService = await EthChainService.newEthChainService(
@@ -29,7 +32,7 @@ describe('test Client', () => {
     // TODO: Pass actor private key
     const store = new MemStore(Buffer.from('key'));
 
-    const client = await Client.new(
+    client = await Client.new(
       messageService,
       chainService,
       store,
@@ -38,8 +41,13 @@ describe('test Client', () => {
     );
 
     expect(client.address).to.equal(ethers.constants.AddressZero);
+  });
 
+  it('should create ledger channel', async () => {
     const response = client.createLedgerChannel('', 0, []);
     expect(response).to.be.empty;
+
+    // TODO: Implement and close services
+    // client.close();
   });
 });
