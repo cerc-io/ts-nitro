c2acea8b6b3052b6175867bc35e71a0829403a13
prathamesh0 <prathamesh.musale0@gmail.com>
2023-05-25 16:33:56 +0530

    Add a client method to create directly funded ledger channel

--- packages/nitro-client/src/client/client.ts
+++ packages/nitro-client/src/client/client.ts
@@ -1,6 +1,9 @@
 import debug from 'debug';
+import assert from 'assert';
+import { ethers } from 'ethers';
 
 import type { ReadWriteChannel } from '@nodeguy/channel';
+import { randUint64 } from '@cerc-io/nitro-util';
 
 import { MessageService } from './engine/messageservice/messageservice';
 import { ChainService } from './engine/chainservice/chainservice';
@@ -14,6 +17,11 @@ import { ObjectiveId } from '../protocols/messages';
 import { SyncMap } from '../internal/safesync/safesync';
 import { Voucher } from '../payments/vouchers';
 import { MetricsApi } from './engine/metrics';
+import { Exit } from '../channel/state/outcome/exit';
+import {
+  ObjectiveResponse as DirectFundObjectiveResponse,
+  ObjectiveRequest as DirectFundObjectiveRequest,
+} from '../protocols/directfund/directfund';
 
 const log = debug('ts-nitro:client');
 
@@ -21,7 +29,7 @@ export class Client {
   // The core business logic of the client
   private engine?: Engine;
 
-  private address?: Address;
+  address?: Address;
 
   private channelNotifier?: ChannelNotifier;
 
@@ -41,15 +49,45 @@ export class Client {
 
   private logger?: debug.Debugger;
 
-  static new(
+  static async new(
     messageService: MessageService,
     chainservice: ChainService,
     store: Store,
     logDestination: WritableStream,
     policymaker: PolicyMaker,
     metricsApi: MetricsApi,
-  ): Client {
+  ): Promise<Client> {
     // TODO: Port over implementation
-    return new Client();
+    const client = new Client();
+
+    client.address = store.getAddress();
+    client.chainId = await chainservice.getChainId();
+    client.vm = new VoucherManager(ethers.constants.AddressZero, store);
+
+    client.engine = new Engine(client.vm, messageService, chainservice, store, policymaker);
+
+    return client;
+  }
+
+  // CreateLedgerChannel creates a directly funded ledger channel with the given counterparty.
+  // The channel will run under full consensus rules (it is not possible to provide a custom AppDefinition or AppData).
+  // TODO: uint32 replacement
+  createLedgerChannel(counterparty: Address, challengeDuration: number, outcome: Exit): DirectFundObjectiveResponse {
+    assert(this.engine);
+    assert(this.address);
+    assert(this.chainId);
+
+    const objectiveRequest = new DirectFundObjectiveRequest(
+      counterparty,
+      challengeDuration,
+      outcome,
+      randUint64(),
+      this.engine.getConsensusAppAddress(),
+    );
+
+    // Send the event to the engine
+    this.engine.objectiveRequestsFromAPI.push(objectiveRequest);
+    objectiveRequest.waitForObjectiveToStart();
+    return objectiveRequest.response(this.address, this.chainId);
   }
 }
