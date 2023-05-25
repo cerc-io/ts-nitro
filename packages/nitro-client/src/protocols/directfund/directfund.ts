e4a0aeb1721e93560d16abce2110e58edad15983
prathamesh0 <prathamesh.musale0@gmail.com>
2023-05-25 16:31:19 +0530

    Add placeholder methods for directfund protocol

---
+++ packages/nitro-client/src/protocols/directfund/directfund.ts
@@ -0,0 +1,62 @@
+import createChannel from '@nodeguy/channel';
+import type { ReadWriteChannel } from '@nodeguy/channel';
+
+import { Exit } from '../../channel/state/outcome/exit';
+import { Address } from '../../types/types';
+import { ObjectiveId } from '../messages';
+
+// ObjectiveRequest represents a request to create a new direct funding objective.
+export class ObjectiveRequest {
+  counterParty: Address;
+
+  // TODO: uint32 replacement
+  challengeDuration: number;
+
+  outcome: Exit;
+
+  appDefinition: Address;
+
+  appData?: Buffer;
+
+  // TODO: uint32 replacement
+  nonce: number;
+
+  private objectiveStarted: ReadWriteChannel<void>;
+
+  constructor(
+    counterParty: Address,
+    challengeDuration: number,
+    outcome: Exit,
+    nonce: number,
+    appDefinition: Address,
+  ) {
+    this.counterParty = counterParty;
+    this.challengeDuration = challengeDuration;
+    this.outcome = outcome;
+    this.nonce = nonce;
+    this.appDefinition = appDefinition;
+    this.objectiveStarted = createChannel();
+  }
+
+  // SignalObjectiveStarted is used by the engine to signal the objective has been started.
+  signalObjectiveStarted(): void { }
+
+  // WaitForObjectiveToStart blocks until the objective starts
+  waitForObjectiveToStart(): void {}
+
+  // Id returns the objective id for the request.
+  id(myAddress: Address, chainId: bigint): ObjectiveId {
+    return '';
+  }
+
+  // Response computes and returns the appropriate response from the request.
+  response(myAddress: Address, chainId: bigint): ObjectiveResponse {
+    return {} as ObjectiveResponse;
+  }
+}
+
+// ObjectiveResponse is the type returned across the API in response to the ObjectiveRequest.
+export type ObjectiveResponse = {
+  id: ObjectiveId
+  channelId: string
+};
