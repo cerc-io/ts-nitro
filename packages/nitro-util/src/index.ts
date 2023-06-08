574fe05e5cc64223c525e391717c7f2098c29ca0
Nabarun <nabarun@deepstacksoft.com>
2023-06-08 19:31:56 +0530

    Use TypeChain generated bindings in ChainService

--- packages/nitro-util/src/index.ts
+++ packages/nitro-util/src/index.ts
@@ -4,3 +4,5 @@ export * from './go';
 export * from './hex-utils';
 export * from './json';
 export * from './contract-bindings';
+
+export { INitroTypes, ExitFormat } from './contract-bindings/NitroAdjudicator';
