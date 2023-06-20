7ce56ed1c20b0615a91006051066268a1b5249b3
neeraj <neeraj.rtly@gmail.com>
2023-06-20 12:15:12 +0530

    Implement classes ChannelNotifier, PaymentChannelListeners, LedgerChannelListeners

--- packages/nitro-client/src/client/notifier/channel-notifier.ts
+++ packages/nitro-client/src/client/notifier/channel-notifier.ts
@@ -2,23 +2,58 @@
 
 import { VoucherManager } from '../../payments/voucher-manager';
 import { Store } from '../engine/store/store';
+import { PaymentChannelListeners, LedgerChannelListeners } from './listeners';
+import { SafeSyncMap } from '../../internal/safesync/safesync';
 import { LedgerChannelInfo, PaymentChannelInfo } from '../query/types';
 
-// TODO: Implement
+const ALL_NOTIFICATIONS = 'all';
+
+// ChannelNotifier is used to notify multiple listeners of a channel update.
 export class ChannelNotifier {
+  private ledgerListeners?: SafeSyncMap<LedgerChannelListeners>;
+
+  private paymentListeners?: SafeSyncMap<PaymentChannelListeners>;
+
+  private store?: Store;
+
+  private vm?: VoucherManager;
+
+  constructor(params: {
+    ledgerListeners: SafeSyncMap<LedgerChannelListeners>;
+    paymentListeners: SafeSyncMap<PaymentChannelListeners>;
+    store: Store;
+    vm: VoucherManager;
+  }) {
+    Object.assign(this, params);
+  }
+
   // NewChannelNotifier constructs a channel notifier using the provided store.
   static newChannelNotifier(store: Store, vm: VoucherManager): ChannelNotifier {
-    // TODO: Implement
-    return new ChannelNotifier();
+    return new ChannelNotifier({
+      ledgerListeners: new SafeSyncMap<LedgerChannelListeners>(),
+      paymentListeners: new SafeSyncMap<PaymentChannelListeners>(),
+      store,
+      vm,
+    });
   }
 
   // NotifyLedgerUpdated notifies all listeners of a ledger channel update.
   // It should be called whenever a ledger channel is updated.
-  // TODO: Implement
-  notifyLedgerUpdated(info: LedgerChannelInfo) {}
+  notifyLedgerUpdated(info: LedgerChannelInfo): void {
+    const [li] = this.ledgerListeners!.loadOrStore(info.iD.string(), LedgerChannelListeners.newLedgerChannelListeners());
+    li.Notify(info);
+
+    const [allLi] = this.ledgerListeners!.loadOrStore(ALL_NOTIFICATIONS, LedgerChannelListeners.newLedgerChannelListeners());
+    allLi.Notify(info);
+  }
 
   // NotifyPaymentUpdated notifies all listeners of a payment channel update.
   // It should be called whenever a payment channel is updated.
-  // TODO: Implement
-  notifyPaymentUpdated(info: PaymentChannelInfo) {}
+  notifyPaymentUpdated(info: PaymentChannelInfo): void {
+    const [li] = this.paymentListeners!.loadOrStore(info.iD.string(), PaymentChannelListeners.newPaymentChannelListeners());
+    li.Notify(info);
+
+    const [allLi] = this.paymentListeners!.loadOrStore(ALL_NOTIFICATIONS, PaymentChannelListeners.newPaymentChannelListeners());
+    allLi.Notify(info);
+  }
 }
