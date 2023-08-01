850ce8b7990b6e0f3e92fe661c0e5b7e0ae17586
neeraj <neeraj.rtly@gmail.com>
2023-08-01 16:20:27 +0530

    Implement waitForMultiplePeers method in server script

--- packages/server/src/utils/index.ts
+++ packages/server/src/utils/index.ts
@@ -9,3 +9,11 @@ export async function waitForPeerInfoExchange(services: P2PMessageService[]) {
     await Promise.all(services.map((service) => service.peerInfoReceived().shift()));
   }
 }
+
+// waitForMultiplePeers waits for all the P2PMessageServices to receive peer info from each other
+export async function waitForMultiplePeers(numOfPeers: number, services: P2PMessageService[]) {
+  for (let i = 0; i < numOfPeers; i += 1) {
+    /* eslint-disable no-await-in-loop */
+    await Promise.all(services.map((service) => service.peerInfoReceived().shift()));
+  }
+}
