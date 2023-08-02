b40372380c9aab6d3ef0330e28653d8d129803ab
prathamesh0 <prathamesh.musale0@gmail.com>
2023-08-02 18:13:32 +0530

    Add scripts to build and deploy a token contract

---
+++ packages/nitro-util/scripts/deploy-token.ts
@@ -0,0 +1,35 @@
+import yargs from 'yargs';
+import debug from 'debug';
+import { providers } from 'ethers';
+
+import { DEFAULT_CHAIN_URL } from '../src';
+import { deployToken } from '../src/deploy-contracts';
+import tokenArtifact from '../artifacts/contracts/Token.sol/Token.json';
+
+const log = debug('ts-nitro:util');
+
+const getArgv = () => yargs.parserConfiguration({
+  'parse-numbers': false,
+}).options({
+  chainurl: {
+    alias: 'c',
+    type: 'string',
+    describe: 'RPC endpoint for the chain',
+    default: DEFAULT_CHAIN_URL,
+  },
+}).argv;
+
+async function main() {
+  const argv = getArgv();
+
+  const provider = new providers.JsonRpcProvider(argv.chainurl);
+  const tokenAddress = await deployToken(provider.getSigner(), tokenArtifact);
+
+  log('Token deployed to:', tokenAddress);
+}
+
+main()
+  .then(() => {})
+  .catch((err) => {
+    log(err);
+  });
