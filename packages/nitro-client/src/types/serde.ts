a6d7c2bc7d9d2e7c08ba5de65f7768415e1c24f0
neeraj <neeraj.rtly@gmail.com>
2023-08-07 10:35:00 +0530

    Use JsonRpcError in getPaymentChannelInfo

--- packages/nitro-client/src/types/serde.ts
+++ packages/nitro-client/src/types/serde.ts
@@ -1,12 +1,13 @@
 export class JsonRpcError extends Error {
-
   private code?: number;
 
   private data?: any;
 
   private id?: number;
 
-  constructor({ message, code, data, id }: { message: string; code?: number; data?: any; id?: number }) {
+  constructor({
+    message, code, data, id,
+  }: { message: string; code?: number; data?: any; id?: number }) {
     super(message);
     this.code = code;
     this.data = data;
@@ -18,10 +19,10 @@ export class JsonRpcError extends Error {
   }
 }
 
-const ParseError = new JsonRpcError({ message: "Parse error", code: -32700 })
-const InvalidRequestError = new JsonRpcError({ message: "Invalid Request", code: -32600 })
-const MethodNotFoundError = new JsonRpcError({ message: "Method not found", code: -32601 })
-const InvalidParamsError = new JsonRpcError({ message: "Invalid params", code: -32602 })
-const InternalServerError = new JsonRpcError({ message: "Internal error", code: -32603 })
-const UnexpectedRequestUnmarshalError = new JsonRpcError({ message: "Could not unmarshal request object", code: -32010 })
-const UnexpectedRequestUnmarshalError2 = new JsonRpcError({ message: "Could not unmarshal params object", code: -32009 })
+export const ParseError = new JsonRpcError({ message: 'Parse error', code: -32700 });
+export const InvalidRequestError = new JsonRpcError({ message: 'Invalid Request', code: -32600 });
+export const MethodNotFoundError = new JsonRpcError({ message: 'Method not found', code: -32601 });
+export const InvalidParamsError = new JsonRpcError({ message: 'Invalid params', code: -32602 });
+export const InternalServerError = new JsonRpcError({ message: 'Internal error', code: -32603 });
+export const UnexpectedRequestUnmarshalError = new JsonRpcError({ message: 'Could not unmarshal request object', code: -32010 });
+export const UnexpectedRequestUnmarshalError2 = new JsonRpcError({ message: 'Could not unmarshal params object', code: -32009 });
