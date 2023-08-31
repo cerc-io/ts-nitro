0624dd48d2c5830d992c6377e140480e1da29d0a
Nabarun Gogoi <nabarun@deepstacksoft.com>
2023-08-31 14:28:44 +0530

    Change Hardhat mining mode to interval mining (#111)
    Co-authored-by: neeraj <neeraj.rtly@gmail.com>

--- packages/server/hardhat.config.ts
+++ packages/server/hardhat.config.ts
@@ -2,6 +2,14 @@ import { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.18',
+  networks: {
+    hardhat: {
+      mining: {
+        auto: false,
+        interval: 5000,
+      },
+    },
+  },
 };
 
 export default config;
